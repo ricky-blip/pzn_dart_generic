cd716b2e10fe21d2e735115a5049edf756a06eb1
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-13 12:59:10 +0700

    create dart generic

--- dart_generic/bin/dart_generic.dart
+++ dart_generic/bin/dart_generic.dart
@@ -1,5 +1,5 @@
 import 'package:dart_generic/dart_generic.dart' as dart_generic;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_generic.calculate()}!');
+  print('Dart Generic: ${dart_generic.calculate()}!');
 }
