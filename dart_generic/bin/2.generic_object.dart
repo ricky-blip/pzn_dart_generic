e60531c4114691ff80bdbe23222b56f2d02af097
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-16 20:23:00 +0700

    update

--- dart_generic/bin/2.generic_object.dart
+++ dart_generic/bin/2.generic_object.dart
@@ -1,5 +1,13 @@
 import '1.generic_class.dart';
 
 void main() {
-  
+  // NOTE cara mengakses generic class menjadi generic object
+  var dataString = MyData<String>("Ricky Rinaldy"); // bisa ditambahkan secara explicit <String>
+  var dataNumber = MyData(99);  //langsung saja masukkan value number
+  var dataBool = MyData(false); //otomatis langsung terdeteksi value yang diberikan
+
+  print(dataString.data);
+  print(dataNumber.data);
+  print(dataBool.data);
+
 }
