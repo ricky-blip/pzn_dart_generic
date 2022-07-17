2270d2a65ee5e3eccbc22fa85b80d7ecdfb34609
ricky-blip <ricky.rinaldy77@gmail.com>
2022-07-17 13:33:16 +0700

    update

---
+++ dart_generic/bin/3.multiple_parameter_type.dart
@@ -0,0 +1,25 @@
+class Pair<R, I, C> {
+  R first;
+  I second;
+  C third;
+
+  Pair(this.first, this.second, this.third);
+}
+
+void main() {
+  var myString = Pair<String, String, int>("Ricky", "Rinaldy", 3);
+  var myNumber = Pair(1.0, 2, 3.00);
+  var myBoolean = Pair(true, false, 3);
+
+  print(myString.first);
+  print(myString.second);
+  print(myString.third);
+
+  print(myNumber.first);
+  print(myNumber.second);
+  print(myNumber.third);
+
+  print(myBoolean.first);
+  print(myBoolean.second);
+  print(myBoolean.third);
+}
